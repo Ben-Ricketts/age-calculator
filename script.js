678cd6fd2ba995a385ad48898d877048ff49a376
Benske4 <128804040+Benske4@users.noreply.github.com>
2023-07-03 20:24:38 +1200

    months and years functionality

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-let day = document.getElementById("dd");
+let ddInput = document.getElementById("dd");
 let days = document.getElementById("days");
+let yyyyInput = document.getElementById("yyyy");
+let years = document.getElementById("years");
+let mmInput = document.getElementById("mm");
+let months = document.getElementById("month");
+let monthsInYear = 12;
 
 document.addEventListener("keydown", function (e) {
-  console.log(e.key);
   if (e.key === "Enter") {
-    console.log(day.value);
-    days.textContent = day.value;
+    days.textContent = ddInput.value;
+    years.textContent = workoutBirthYear();
+    months.textContent = monthsInYear - mmInput.value;
   }
 });
+let currentYear = new Date().getFullYear();
+
+let workoutBirthYear = function () {
+  let birhtYear = currentYear - yyyyInput.value;
+  return birhtYear;
+};
+
+// months with 31 days = jan,march, may, july, august, oct, dec
+//months with 30 days = april, june, sep, nov
+//28 days= feb. leap years = 2020, 2016 and every 4 years
